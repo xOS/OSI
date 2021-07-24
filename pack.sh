cf6d44697900b0c1dab61b06304d352f36fe8a60
佩佩 <admin@nange.cn>
2021-07-24 20:20:57 +0800

    Update.

--- pack.sh
+++ pack.sh
@@ -3,8 +3,8 @@ set -e
 
 tag=$(git describe --tags)
 npm pack
-tar xf font-logos*.tgz
-mv package font-logos-$tag
-zip -r font-logos-$tag.zip font-logos-$tag
-rm -r font-logos-$tag
-rm font-logos*.tgz
+tar xf osi*.tgz
+mv package osi-$tag
+zip -r osi-$tag.zip osi-$tag
+rm -r osi-$tag
+rm osi*.tgz
